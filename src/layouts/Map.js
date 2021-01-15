eb1aaf881d05cfbac78cb519fc575b20be5bcdb6
Dmitriy Kryukov <kriukovdmitriy@gmail.com>
2021-01-15 01:38:58 -0800

    Add structure folder and files

---
+++ src/layouts/Map.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Map() {
+  return (
+    <div class='wrapper'>
+        <header class="header">HEADER</header>
+        <div class='mainPanel'>
+          
+          <div class='content'>
+            CONTENT
+          </div>
+          
+        </div>
+      </div>
+  )
+}
